e62b87f8d2987f3acb7c7d8e5bf4ebf53b8a9abd
dldoglaslopes <dldouglaslopes@gmail.com>
2020-01-15 18:49:20 -0300

    Add categoria class + pessoa class

---
+++ src/main/java/com/example/algaworks/api/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.example.algaworks.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.algaworks.api.model.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+
+}
---
+++ src/main/java/com/example/algaworks/api/resource/PessoaResource.java
@@ -0,0 +1,60 @@
+package com.example.algaworks.api.resource;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.algaworks.api.model.Pessoa;
+import com.example.algaworks.api.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@GetMapping
+	public List<Pessoa> listar(){
+		return pessoaRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa,
+											HttpServletResponse response){
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+											.path("/{codigo}")
+											.buildAndExpand(pessoaSalva.getCodigo())
+											.toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
+		
+		return pessoaSalva.isPresent()?
+				ResponseEntity.ok(pessoaSalva) : ResponseEntity.notFound().build();
+	}
+}
